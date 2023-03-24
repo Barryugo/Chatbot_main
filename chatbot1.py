861ee37a4916806e568ebc5b5da01c1088fd9036
Barry <106887760+Barryugo@users.noreply.github.com>
2023-03-24 01:50:57 +0100

    Add files via upload

---
+++ chatbot1.py
@@ -0,0 +1,80 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+# Make a request to the ChatCompletion API
+openai.api_key = "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"
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+
+def chatbot(question):
+    # define the conversation context based on the input question
+    if "world series" in question:
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant created by Barry."},
+            {"role": "user", "content": question},
+            {"role": "assistant",
+                "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    else:
+        # default conversation context if question doesn't match any specific case
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant created by Barry."},
+            {"role": "user", "content": question}
+        ]
+
+    # generating the response based on the conversation context
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.1,
+        presence_penalty=0.6,
+        stop=[" Human:", "AI:"]
+    )
+
+    # return the generated response
+    return response["choices"][0]["message"]["content"]
+
+
+# Set up Streamlit app
+st.title("JRZY Chatbot")
+
+# Initialize session state for message history
+if "message_history" not in st.session_state:
+    st.session_state.message_history = []
+
+placeholder = st.empty()  # a placeholder container to display the messages
+
+question = st.text_input("Ask me your questions:", "")
+
+if st.button("Ask"):
+    response = chatbot(question)  # get response from the chatbot
+
+    # append the user's question and the chatbot's response to the message history
+    st.session_state.message_history.append(question)
+    st.session_state.message_history.append(response)
+
+    # display the message history using the placeholder container
+    with placeholder.container():
+        for msg in st.session_state.message_history:
+            # check if the message is from the user or the chatbot
+            if msg == question or msg.startswith("You:"):
+                message(msg, is_user=True)  # display the user's message
+            else:
+                message(msg, is_user=False)  # display the chatbot's message
+
+
+def message(msg, is_user):
+    if is_user:
+        st.write(
+            f'<div style="text-align: right; color: blue;">You: {msg}</div>', unsafe_allow_html=True)
+    else:
+        st.write(
+            f'<div style="text-align: left; color: green;">Chatbot: {msg}</div>', unsafe_allow_html=True)
+    st.write('<br>', unsafe_allow_html=True)
